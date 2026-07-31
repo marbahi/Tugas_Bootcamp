79d0a9252e56b0d52946df64fb8da68ddb0272e9
marbahi <ammar.skyagt@gmail.com>
2026-07-31 19:18:18 +0700

    Menambahkan Tugas 11'

---
+++ Tugas_11/resources/views/home.blade.php
@@ -0,0 +1,6 @@
+@extends('template.body')
+
+@section('content')
+    <h1>Home Page</h1>
+    <p>Ini adalah halaman Home</p>
+@endsection
---
+++ Tugas_11/resources/views/template/body.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E-Commerce</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body class="d-flex flex-column min-vh-100">
+    @include('template.header')
+    <main class="flex-grow-1 py-4 mx-4">@yield('content')</main>
+    @include('template.footer')
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
